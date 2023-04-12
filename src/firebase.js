c240aac75cc087f7bcb74c62f352dbe87a714933
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2023-04-12 14:00:09 +0000

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRJrac1m4VQjkZ3EdkXct4mZ2FNvOnHQs",
+  authDomain: "react-file-upload-b7944.firebaseapp.com",
+  projectId: "react-file-upload-b7944",
+  storageBucket: "react-file-upload-b7944.appspot.com",
+  messagingSenderId: "1053234426676",
+  appId: "1:1053234426676:web:cbf8a9fe61a3169166eb61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
